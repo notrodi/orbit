2201a884feee46337728ac7244ef6354c62764fa
notrodi <nnotrodi1@gmail.com>
2026-09-23 21:44:54 +0300

    refactor: update `App` root component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-    <h1>Hello, {{ title() }}</h1>
-
-    <router-outlet />
-  `,
+  template: '<router-outlet />',
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('orbit');
-}
+export class App {}
